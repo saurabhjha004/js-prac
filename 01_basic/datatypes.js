484c047d2f01500e5ed97b02e77a145cd2956c5f
saurabhjha004 <saurabhdkmjha@gmail.com>
2024-06-01 12:13:16 +0530

    code added

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") //we are using node js not browser
+
+let name = "saurabh"    //string
+let age = 20    //int...can also use bigint
+let isLoggedIn = false  //boolean
+let state   //undefined
+let state1 = null   //null => standalone value...its datatype is called object
+
+console.log(typeof age)
+console.log(typeof state1);
